9597114d559661c059eedf0a5e7e9e050dd258c4
Francisco Caballe <francaballe@gmail.com>
2026-03-15 13:27:23 -0300

    arreglo de bug de popups en notebooks?

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1617,7 +1617,7 @@ export default function SettingsPage() {
                                     <div className={styles.settingInfo}>
                                         <span className={styles.settingLabel}>ℹ️ {language === 'es' ? 'Versión del Software' : 'Software Version'}</span>
                                         <span className={styles.settingDescription}>
-                                            v1.11.1
+                                            v1.11.2
                                         </span>
                                     </div>
                                 </div>
